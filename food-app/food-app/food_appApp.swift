8335731b73e5a8190b87a7d21bb612820f408aae
Marina Ryabchun <marinarrrr@gmail.com>
2023-07-03 11:30:50 +0300

    Create empty project

---
+++ food-app/food-app/food_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_appApp.swift
+//  food-app
+//
+//  Created by Марина Рябчун on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct food_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
